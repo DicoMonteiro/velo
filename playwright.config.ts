3bdbd249eff410157ebb042b6748ea49b8988390
Adriano Almeida <adrianobma@gmail.com>
2026-05-06 07:53:15 -0300

    build e deploy no vercel com o secret de token correto

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
     // Quando o valor é 0, herda o limite do timeout geral do teste
     navigationTimeout: 10_000,
 
-    baseURL: 'https://velo-dicomonteiro.vercel.app'
+    baseURL: 'https://velo-489xp09yr-dicomonteiros-projects.vercel.app'
   },
 
   /* Configure projects for major browsers */
